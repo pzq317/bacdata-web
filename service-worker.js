8d92c746a9e8c1311c8d16c3ae548b45358d63a6
pzq317 <pzq317@gmail.com>
2018-11-27 20:12:38 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bacdata-web/precache-manifest.6a4aa469f41b641fc2360b0895141f85.js"
+  "/bacdata-web/precache-manifest.acd5c2a2e5cf693276351ed9fdb8d816.js"
 );
 
 workbox.clientsClaim();
